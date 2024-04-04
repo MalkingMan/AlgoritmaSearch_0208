3a99248930c08d226649b51b6686e08591c9fff0
MalkingMan <arrayalkhozini@gmail.com>
2024-04-04 14:56:58 +0700

    commit 16

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -53,6 +53,8 @@ if (i == n) // step 5
 cout << "\n" << item << " not found in the array\n";
 cout << "\nNumber of comparisons: " << ctr << endl;
 
+cout << "\nContinue search (y/n): ";
+cin >> ch;
 
 
 
