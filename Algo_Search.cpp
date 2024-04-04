f4cdd7ffb234816aebdefda1b096200c2ebabe0a
MalkingMan <arrayalkhozini@gmail.com>
2024-04-04 15:05:56 +0700

    commit 19

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -51,19 +51,27 @@ void LinearSearch()
                 break;
             }
             i++; // step 4 
-         }
+        }
 
-    if (i == n) // step 5
-    cout << "\n" << item << " not found in the array\n";
-    cout << "\nNumber of comparisons: " << ctr << endl;
+        if (i == n) // step 5
+        cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
 
-    cout << "\nContinue search (y/n): ";
-    cin >> ch;
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
 
 
     } while ((ch == 'y') || (ch == 'Y'));
 
 } 
+
+void display()
+{
+cout << "Array elements are: "; for (i = 0; i < n; i++)
+cout << arr[i] << " ";
+cout << endl;
+
+}
 
 
 
