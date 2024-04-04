6d5a793a745cde0f4cc247946b0371e58917ee67
MalkingMan <arrayalkhozini@gmail.com>
2024-04-04 14:41:46 +0700

    commt 6

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -13,4 +13,9 @@ int i; // Index of array element
         cin >> n;
         if ((n > 0) && (n <= 20))
             break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
+
+    
 
